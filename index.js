42961c0039b4c95640cea74cc5380e3f962fac05
Alex Holhos <alex.holhos@emanuel.ro>
2018-05-19 10:35:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setCart(c) {
 function addToCart(item) {
   var price = Math.floor(Math.random() * 100);
   cart.push({ 'itemName':item, 'itemPrice':price});
-  itemName = item;
+  var itemName = item;
   return itemName + 'has been added to the cart';
  
 }
